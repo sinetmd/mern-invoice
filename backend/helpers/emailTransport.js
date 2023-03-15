f6011cd9776a8113b5e70b04fb82beebd2c8cc43
sinetmd <dobremrn12@gmail.com>
2023-03-15 06:56:58 +0200

    chore: configure nodemail email helper

---
+++ backend/helpers/emailTransport.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import nodemailer from "nodemailer";
+
+let transporter;
+
+if (process.env.NODE_ENV === "developement") {
+  transporter = nodemailer.createTransport({
+    host: "mailhog",
+    port: 1025,
+  });
+} else if (process.env.NODE_ENV === "production") {
+  transporter = nodemailer.createTransport({
+    // TODO: configure mailgun in production
+  });
+}
+
+export default transporter;
