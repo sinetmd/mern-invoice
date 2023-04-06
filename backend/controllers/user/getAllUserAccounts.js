d12ff3a41e6362338a4b046db1552b4777d6c748
sinetmd <dobremrn12@gmail.com>
2023-04-06 16:28:17 +0300

    feat: get all user accounts/profiles - admin only

---
+++ backend/controllers/user/getAllUserAccounts.js
@@ -0,0 +1,31 @@
+import asyncHandler from "express-async-handler";
+import User from "../../models/userModel.js";
+
+// $-title   Get All Users
+// $-path    GET /api/v1/user/all
+// $-auth    Private/Admin
+
+const getAllUserAccounts = asyncHandler(async (req, res) => {
+  const pageSize = 10;
+
+  const page = Number(req.query.pageNumber) || 1;
+
+  // get all counted users
+  const count = await User.countDocuments({});
+
+  const users = await User.find()
+    .sort({ createdAt: -1 })
+    .select("-refreshToken")
+    .limit(pageSize)
+    .skip(pageSize * (page - 1))
+    .lean();
+
+  res.json({
+    success: true,
+    count,
+    numberOfPages: Math.ceil(count / pageSize),
+    users,
+  });
+});
+
+export default getAllUserAccounts;
